35da2aac0b2b9b60bbb9a50d4fd38a556eb15b36
riggs <riggs@naver.com>
2020-07-01 17:44:06 +0900

    Updates

---
+++ precache-manifest.779f1eb4d6836b021ab9de6f0d2096e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d24ab8dbb59dbf9bc7490185cba5113",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "30221cdc28c5e76a5e3b",
+    "url": "/movie_app_2020/static/css/main.e399bba3.chunk.css"
+  },
+  {
+    "revision": "2ace96f11a118a1963a4",
+    "url": "/movie_app_2020/static/js/2.23fdfa78.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.23fdfa78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30221cdc28c5e76a5e3b",
+    "url": "/movie_app_2020/static/js/main.f0a99d56.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
